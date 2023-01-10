ac237639cfa7cef5ac423b70a24e8a8210c770f7
Mike Kinney <mike.kinney@gmail.com>
2023-01-09 21:44:23 -0800

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 # This call to setup() does all the work
 setup(
     name="kinneyotpgui",
-    version="0.0.3",
+    version="0.0.4",
     description="Graphical user interface for one time pad",
     long_description=long_description,
     long_description_content_type="text/markdown",
