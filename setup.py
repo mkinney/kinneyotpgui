f50760e75057398d09a156ef699a677c1cf064a1
Mike Kinney <mike.kinney@gmail.com>
2023-01-09 20:32:35 -0800

    bump rev after select/copy encoded/decoded text

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 # This call to setup() does all the work
 setup(
     name="kinneyotpgui",
-    version="0.0.2",
+    version="0.0.3",
     description="Graphical user interface for one time pad",
     long_description=long_description,
     long_description_content_type="text/markdown",
